64f811a8c5ea6ec45b8732a344fb5a3c2ab27c9e
Ahmed Magdy <mo3gza0@gmail.com>
2023-05-20 15:45:37 +0300

    Task

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded =['id'];
+
+
+    public function comment(): HasMany
+    {
+        return $this->hasMany(comment::class);
+    }
+
+}
